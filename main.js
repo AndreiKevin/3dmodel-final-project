11cec2ffedcb5f07943a83c1b47ae5cd6072291e
AndreiKevin <81185469+AndreiKevin@users.noreply.github.com>
2024-02-19 15:01:52 +0800

    add: mini1

--- main.js
+++ main.js
@@ -1,30 +1,6 @@
 import * as THREE from 'three';
+import mini1 from './mini1.js';
+import cube from './cube.js';
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 100);
-const renderer = new THREE.WebGLRenderer();
-
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
-
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0xdb4045 });
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
-
-// Set camera position
-camera.position.z = 5;
-
-// Add light source
-const light = new THREE.PointLight(0xffffff, 1);
-light.position.set(0, 0, 5);
-scene.add(light);
-
-function animate() {
-    requestAnimationFrame(animate);
-    cube.rotation.x += 0.01;
-    cube.rotation.y -= 0.01; // negative angle is clockwise
-    cube.rotation.z += 0.01;
-    renderer.render(scene, camera);
-}
-animate();
+//cube();
+mini1();
---
+++ mini1.js
@@ -0,0 +1,42 @@
+import * as THREE from 'three';
+
+export default function mini1() {
+    // basic initializations
+    const renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+    const scene = new THREE.Scene();
+    const camera = new THREE.OrthographicCamera(-2, 2, 2, -2, 1, 1000);
+    
+    // helpers
+    const axesHelper = new THREE.AxesHelper(5);
+    scene.add(axesHelper);
+
+    // objects
+    const cubeGeometry = new THREE.BoxGeometry();
+    const cubeMaterial = new THREE.MeshBasicMaterial({color: 0xff0000});
+    const cubeMesh = new THREE.Mesh(cubeGeometry, cubeMaterial);
+    cubeMesh.position.set(1, 1, -5); 
+    scene.add(cubeMesh);
+
+    const torusGeometry = new THREE.TorusGeometry(0.45, 0.1);
+    const torusMaterial = new THREE.MeshBasicMaterial({color: 0xffe24f});
+    const torusMesh = new THREE.Mesh(torusGeometry, torusMaterial);
+    torusMesh.position.set(-1, 1, -5); 
+    scene.add(torusMesh);
+
+    const sphereGeometry = new THREE.SphereGeometry(0.6);
+    const sphereMaterial = new THREE.MeshBasicMaterial({color: 0x00ff00});
+    const sphereMesh = new THREE.Mesh(sphereGeometry, sphereMaterial);
+    sphereMesh.position.set(1, -1, -5);
+    scene.add(sphereMesh);
+
+    const octahedronGeometry = new THREE.OctahedronGeometry(0.6, 1);
+    const octahedronMaterial = new THREE.MeshBasicMaterial({color: 0x1c31ed});
+    const octahedronMesh = new THREE.Mesh(octahedronGeometry, octahedronMaterial);
+    octahedronMesh.position.set(-1, -1, -5);
+    scene.add(octahedronMesh);
+
+    // rendering
+    renderer.render(scene, camera);
+}
